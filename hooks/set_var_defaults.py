d80ea37296aba249107270874ac8a81164cc9dd4
EdGeber <egeber16@gmail.com>
2026-01-11 18:57:13 -0300

    Initial commit

---
+++ hooks/set_var_defaults.py
@@ -0,0 +1,65 @@
+import io
+import sys
+import argparse
+from typing import Sequence
+from ruamel.yaml import YAML
+
+UNITY_HEADER = (
+"""%YAML 1.1
+%TAG !u! tag:unity3d.com,2011:
+--- !u!114 &11400000
+"""
+)
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    yaml.indent(mapping=2, sequence=4, offset=2)
+
+    retval = 0
+    for filename in args.filenames:
+        try:
+            with open(filename, 'r', encoding='utf-8') as f:
+                no_header = "".join(f.readlines()[3:])
+                data = list(yaml.load_all(no_header))
+
+            modified = False
+            for doc in data:
+                container = doc.get('MonoBehaviour') if isinstance(doc, dict) else None
+                
+                if not container or not all(k in container for k in ('isReadonly', 'defaultValue', 'currentValue', 'newValue')):
+                    continue
+                
+                if container['newValue'] != None:
+                    container['newValue'] = None
+                    modified = True
+                
+                if str(container['isReadonly']).strip() == "1":
+                    continue
+                
+                d_val = container['defaultValue']
+                if container['currentValue'] == d_val:
+                    continue
+                container['currentValue'] = d_val
+                modified = True
+
+            if modified:
+                with open(filename, 'w', encoding='utf-8') as f:
+                    stream = io.StringIO()
+                    yaml.dump(data[0], stream)
+                    content = stream.getvalue()
+                    f.write(UNITY_HEADER + content)
+                retval = 1
+
+        except Exception as e:
+            print(f"Error processing {filename}: {e}")
+            continue
+
+    return retval
+
+if __name__ == "__main__":
+    raise SystemExit(main(sys.argv[1:]))
---
+++ tests/set_var_defaults_test.py
@@ -0,0 +1,45 @@
+from hooks.set_var_defaults import main
+from testing.util import get_resource_path
+
+
+def test_game_var_not_pass():
+    orig_path = get_resource_path('MouseSensibility.asset')
+    expected_path = get_resource_path('MouseSensibility_expected.asset')
+    with open(orig_path, 'rb') as f:
+        orig_content = f.read()
+    try:
+        ret = main([orig_path])
+        assert ret == 1
+        with open(orig_path, 'r') as f:
+            changed_content = f.read()
+        with open(expected_path, 'r') as f:
+            expected_content = f.read()
+        assert changed_content.strip() == expected_content.strip()
+    finally:
+        pass
+        with open(orig_path, 'wb') as f:
+            f.write(orig_content)
+
+
+def test_game_var_pass():
+    orig_path = get_resource_path('Name.asset')
+    with open(orig_path, 'rb') as f:
+        orig_content = f.read()
+    try:
+        ret = main([orig_path])
+        assert ret == 0
+    finally:
+        with open(orig_path, 'wb') as f:
+            f.write(orig_content)
+
+
+def test_not_game_var():
+    orig_path = get_resource_path('TEEUUID.asset')
+    with open(orig_path, 'rb') as f:
+        orig_content = f.read()
+    try:
+        ret = main([orig_path])
+        assert ret == 0
+    finally:
+        with open(orig_path, 'wb') as f:
+            f.write(orig_content)
